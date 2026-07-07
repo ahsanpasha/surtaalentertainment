e1f47fecb774e6247005ed312a5843e074253ade
Ahsan <ahsanfiazpasha@gmail.com>
2026-07-07 13:06:09 +0500

    good

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,26 @@ import "./page.css";
 
 export default function HomePage() {
   return (
-    <div className="MainHome">
-      <div className="Celebrate">
-        <div className="CircleDiv"></div>
-        <p className="Celebratetext">Celebrate Culture & Music</p>
+    <main>
+      <div className="MainHome">
+        <div className="Celebrate">
+          <div className="CircleDiv"></div>
+          <p className="Celebratetext">Celebrate Culture & Music</p>
+        </div>
+        <p className="FeelText">
+          Feel the Rhythm with <span>Surtaal</span>.
+        </p>
+        <p className="Leteverytext">
+          Let every beat inspire your soul as Surtaal brings music, energy, and
+          passion together in perfect harmony.
+        </p>
+        <button className="SeeHowbtn">
+          See How it Works
+          <img src="/Images/Navbar/arrow.svg" alt="" />
+        </button>
       </div>
-      <p className="FeelText">
-        Feel the Rhythm with <span>Surtaal</span>.
-      </p>
-      <p className="Leteverytext">
-        Let every beat inspire your soul as Surtaal brings music, energy, and
-        passion together in perfect harmony.
-      </p>
-      <button className="SeeHowbtn">
-        See How it Works
-        <img src="/Images/Navbar/arrow.svg" alt="" />
-      </button>
-    </div>
+
+      {/* what we offer */}
+    </main>
   );
 }
