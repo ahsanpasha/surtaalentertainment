9e32c57a76497a65c463c4b6bf815919a00169eb
Ahsan <ahsanfiazpasha@gmail.com>
2026-07-14 12:49:02 +0500

    aboutus

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -39,20 +39,122 @@ export default function AboutUsPage() {
             <div className="CircleServices"></div>
             <p className="ServicesText">our services</p>
           </div>
-          <p className="BringingText">
-            We are bringing <span>Culture</span> & <span>Music</span> to Life
+          <p className="ArtisticText">
+            The <span>Artistic</span> Journey of <span> Surtaal </span>
+            Entertainment<span> USA</span>
           </p>
-          <p className="AboutDesc">
-            Surtaal Entertainment brings the vibrant energy of South Asian music
-            and culture to life. From intimate gatherings to large-scale
-            productions, we create unforgettable experiences that fuse tradition
-            with contemporary artistry. Our passion lies in curating soulful
-            performances that captivate audiences across the globe. With a
-            diverse network of renowned musicians, dancers, and creative
-            professionals, we deliver seamless, high-impact events designed to
-            leave lasting impressions. Whether it’s a concert, wedding show, or
-            corporate gala — Surtaal turns every moment into a celebration.
+          <p className="AboutUSDesc">
+            <span style={{ fontFamily: "Montserrat-Bold" }}>
+              Mr. Ashkenaz Vincent
+            </span>{" "}
+            has been a student of music his entire life later evolving into a
+            teacher of music as well. He has been on the forefront of promoting
+            Arts and Culture events in the US for the past decade. He proudly
+            runs Surtaal Entertainment USA as an events company.
+            <br />
+            Having first performed at the age of five on Pakistan Television
+            PTV, Vincent knew the journey would be never-ending. He has since
+            been in the company of several esteemed and world-renowned artists
+            and musicians, further solidifying his talent by performing
+            alongside one of the top percussionists in the world – Tabla Maestro
+            Ustad Abdul Sattar Taari Khan. He also performed and represented
+            Pakistani Sufi Music at the 47th annual Festival Of The Nations
+            alongside 23 other nations at the Empire State Convention Center in
+            2018. Vincent continues to inspire emerging talent and champion them
+            through showcases and various events. Surtaal Entertainment USA is a
+            testament to his respect and dedication towards the Arts & Culture
+            of South Asia.
           </p>
+        </div>
+      </div>
+
+      <div className="elevatingdiv">
+        <div className="elevatingdivheader">
+          <div className="linediv"></div>
+          <div className="elevatedivmain">
+            <img
+              src="/Images/AboutUs/arrow.svg"
+              className="arrowdivabout"
+              alt=""
+            />
+            <p className="elevatingText">ELEVATING ENTERTAINMENT</p>
+          </div>
+          <div className="linediv"></div>
+          <p className="elevatingText-normal-new">ELEVATING ENTERTAINMENT</p>
+          <div className="linediv"></div>
+        </div>
+        <div className="acrossnorthdiv">
+          <div
+            className="ServicesDiv"
+            style={{
+              background: "#FFFFFF1C",
+              boxShadow: "0px 4px 15px 0px #00000040",
+            }}
+          >
+            <div className="CircleServices"></div>
+            <p className="ServicesText" style={{ color: "#FFFFFF" }}>
+              our services
+            </p>
+          </div>
+          <p className="ElevatingEntertainmentText">
+            Elevating <span>Entertainment</span>
+          </p>
+          <p className="AboutUSDesc-new">
+            We specialize in Corporate and Commercial Events, Concerts, Musical
+            Events and talent shows in North America, to bring together
+            audiences and the entertainment culture.
+            <br />
+            With A-list superstars, World-Class musicians and emerging talent
+            under our roster, Surtaal Entertainment USA aims to deliver a
+            seamless experience for artists, fans, audiences and clients
+            involved.
+            <br />
+            Our commitment to providing exceptional customer service means we
+            offer a range of VIP packages and perks to enhance your experience
+            even further. From meet-and-greets with the artists to exclusive
+            access to soundchecks and more, we strive to make every event an
+            unforgettable experience for our valued patrons.
+          </p>
+        </div>
+      </div>
+      <div className="OurStorySection">
+        <div className="storyDiv" style={{ width: "fit-content" }}>
+          <div className="CircleServices"></div>
+          <p className="storytext">our story</p>
+        </div>
+        <p className="storyline">
+          Music Unites Here with <span>Surtaal</span>
+        </p>
+        <p className="foundedtext">
+          Founded in 2019, Surtaal Entertainment USA came into being with the
+          sole purpose of promoting the best of the best in the music world
+          through live events in North America.
+        </p>
+        <div className="SuccessContainer">
+          <div className="SuccessDiv">
+            <p className="SuccessNum">
+              7<span>+</span>
+            </p>
+            <p className="SuccessLabel">Years Experience</p>
+          </div>
+          <div className="SuccessDiv">
+            <p className="SuccessNum">
+              100<span>+</span>
+            </p>
+            <p className="SuccessLabel">Successful Event</p>
+          </div>
+          <div className="SuccessDiv">
+            <p className="SuccessNum">
+              50<span>+</span>
+            </p>
+            <p className="SuccessLabel">Exclusive Artists</p>
+          </div>
+          <div className="SuccessDiv">
+            <p className="SuccessNum">
+              60<span>+</span>
+            </p>
+            <p className="SuccessLabel">Professional Team</p>
+          </div>
         </div>
       </div>
     </main>
